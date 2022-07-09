b17413b94e7a862c22570610aeab7946d80f5867
yourusername <wah21001@byui.edu>
2022-07-09 14:57:09 -0600

    create moveAction file

---
+++ Final.Project/Scripting/MoveActorsAction.cs
@@ -0,0 +1,13 @@
+namespace Final.Project.Scripting 
+{
+
+    public abstract class MoveActorsAction : Action
+    {
+
+
+        public override void Execute(Scene scene, float deltaTime, IActionCallback callback)
+        {
+
+        }
+    }
+}
