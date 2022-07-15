53a99ef2aa28e9ac153cfb08bc10e344eb66c714
BJSchliep <sch16104@byui.edu>
2022-07-15 09:00:32 -0600

    Updated

--- Final.Project/Scripting/EnemyCollision.cs
+++ Final.Project/Scripting/EnemyCollision.cs
@@ -6,35 +6,37 @@ using Byui.Games.Services;
 
 namespace Final.Project 
 {
-    public class EnemyCollision 
+    public class EnemyCollision : Byui.Games.Scripting.Action
     {
         private IKeyboardService _keyboardService;
         private IAudioService _audioService;
         private ISettingsService _settingsService;
+
         public EnemyCollision(IServiceFactory serviceFactory)
         {
             _keyboardService = serviceFactory.GetKeyboardService();
             _audioService = serviceFactory.GetAudioService();
             _settingsService = serviceFactory.GetSettingsService();
         }
-        public void CollideEnemies(List<Actor> actors, Actor actor, Scene scene)
+
+        public override void Execute(Scene scene, float deltaTime, IActionCallback callback)
         {
-            foreach (Actor enemy in scene.GetAllActors("enemies"))
+            try
             {
-                if (actor.Overlaps(enemy))
-                enemy.MoveTo(0, 0); // Tell it to randomly move to a location on the screen.
+                Actor actor = scene.GetFirstActor("actors");
+                foreach (Actor enemy in scene.GetAllActors("enemies"))
+                {
+                    if (actor.Overlaps(enemy))
+                    enemy.MoveTo(100, 100); // Tell it to randomly move to a location on the screen.
+                }
+        
+                
+
             }
-        }
-        
-        
-        private bool CheckCollision(List<Actor> actors, Actor actor)
-        {
-            foreach (Actor item in actors)
+            catch (Exception exception)
             {
-                if (actor.Overlaps(item))
-                return true;
+                callback.OnError("Couldn't move actor.", exception);
             }
-            return false;
         }
     }
 }
