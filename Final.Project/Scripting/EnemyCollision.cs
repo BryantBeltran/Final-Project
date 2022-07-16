8c0235ac0c17e467f2f8868f7e83b757de92b9a1
JD Sanchez <67755598+JDSanchz@users.noreply.github.com>
2022-07-16 11:05:21 -0600

    Doneso 0.8

--- Final.Project/Scripting/EnemyCollision.cs
+++ Final.Project/Scripting/EnemyCollision.cs
@@ -17,25 +17,33 @@ namespace Final.Project
             _keyboardService = serviceFactory.GetKeyboardService();
             _audioService = serviceFactory.GetAudioService();
             _settingsService = serviceFactory.GetSettingsService();
+            
         }
 
         public override void Execute(Scene scene, float deltaTime, IActionCallback callback)
-        {
+        {  
+
             try
             {
                 Random rnd = new Random();
                 int numx = rnd.Next(100, 600);
                 int numy = rnd.Next(50, 1200);
-                Actor actor = scene.GetFirstActor("actors");
+                // Actor actor = scene.GetFirstActor("actors");
+                Image actor = (Image) scene.GetFirstActor("actors");
                 foreach (Actor enemy in scene.GetAllActors("enemies"))
                 {
-                    if (actor.Overlaps(enemy))
-                    enemy.MoveTo(numy, numx); // Tell it to randomly move to a location on the screen.
+                    if (actor.Overlaps(enemy)){
+                    enemy.MoveTo(numy, numx); 
+                    // Tell it to randomly move to a location on the screen.
                 }
-        
+                }
+                foreach (Actor fireball in scene.GetAllActors("fireballs")){
+                    if (actor.Overlaps(fireball)){
+                    ; 
+                }
                 
 
-            }
+            }}
             catch (Exception exception)
             {
                 callback.OnError("Couldn't move actor.", exception);
